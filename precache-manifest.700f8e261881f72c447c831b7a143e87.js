18bf3aea0a81229ccf7df12ba574c7b7ee4e4e88
Alexandros Mironidis <alex.m@novoville.ocm>
2020-04-29 12:58:05 +0300

    Updates

---
+++ precache-manifest.700f8e261881f72c447c831b7a143e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0d652b468a441f39bd6e44bf286929",
+    "url": "//alxmrd.github.io/react-cv/index.html"
+  },
+  {
+    "revision": "668884c868d94b2fa095",
+    "url": "//alxmrd.github.io/react-cv/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d3fdd29d71e3444fddf1",
+    "url": "//alxmrd.github.io/react-cv/static/js/2.29832a87.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "//alxmrd.github.io/react-cv/static/js/2.29832a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668884c868d94b2fa095",
+    "url": "//alxmrd.github.io/react-cv/static/js/main.6703f8c3.chunk.js"
+  },
+  {
+    "revision": "a23332b78ab9e966031a",
+    "url": "//alxmrd.github.io/react-cv/static/js/runtime-main.3f7ca9dd.js"
+  }
+]);
