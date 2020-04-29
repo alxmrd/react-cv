bc2568dfb2ac56867abcea7a5dec6780ded99e9f
Alexandros Mironidis <alex.m@novoville.ocm>
2020-04-29 15:50:20 +0300

    Updates

---
+++ precache-manifest.7c4ff178ea0edde511206c3ebc551512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33c17a173b8e18ef58af42f508d5791",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "e920f24688ef64c1e668",
+    "url": "/react-cv/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9844cdcab68dbfad7207",
+    "url": "/react-cv/static/js/2.3848926f.chunk.js"
+  },
+  {
+    "revision": "5e5df85af7fa4e94b7507664f7da034f",
+    "url": "/react-cv/static/js/2.3848926f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e920f24688ef64c1e668",
+    "url": "/react-cv/static/js/main.ff39b5cf.chunk.js"
+  },
+  {
+    "revision": "3e7bd43248cfbb725952",
+    "url": "/react-cv/static/js/runtime-main.54fe3744.js"
+  },
+  {
+    "revision": "dfc965f24ac408d2b740790eafd1948b",
+    "url": "/react-cv/static/media/avatar.dfc965f2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6537ed6d84b5caab8ce5b79aabcec63.js"
+  "/react-cv/precache-manifest.7c4ff178ea0edde511206c3ebc551512.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
